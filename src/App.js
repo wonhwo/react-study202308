bae686ed0d76e3e4004e8fa94edec4f44cc3e6b4
Reppy <dlgy9714@gmail.com>
2023-08-10 14:50:57 +0900

    동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import './App.css';
-import AppItem from './AppItem';
-import Noname from './Noname';
-import Hello from './Hello';
 import ExpenseItem from './components/ExpenseItem';
 
 const App=() =>{
   return (
     <>
-    <AppItem/>
-    <Hello/>
-   <Noname/>
-
+    <ExpenseItem/>
     </>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,33 @@
 import React from 'react'
-
+// css 로딩
+import './ExpenseItem.css';
 const ExpenseItem = () => {
+    const expenseDate = new Date(2023,8,10);
+    const expenseTitle = '냠냠';
+    const expensePrice = 19000;
+
+    // 1자리 숫자를 2자리수로 변환하는 함수
+    const make2digit = text =>{
+        return text.toString().padStart(2,'0');
+    }
+
+    // 날짜 포맷팅 변환 함수 정의
+    const makeFormattedDate = ()=>{
+        const year = expenseDate.getFullYear();
+        const month = expenseDate.getMonth();
+        const date = expenseDate.getDate();
+        return `${year}년 ${make2digit(month)}월 ${make2digit(date)}일`
+    };
+
+    // 숫자를 원화표기법으로 바꾸기
+    const formattedPrice = new Intl.NumberFormat('ko-KR').format(expensePrice);
+
   return (
     <div className="expense-item">
-      <div>2023년 08월 10일</div>
+      <div>{makeFormattedDate()}</div>
       <div className="expense-item__description">
-        <h2>냠냠치킨</h2>
-        <div className="expense-item__price">19000원</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{formattedPrice}</div>
       </div>
     </div>
     
